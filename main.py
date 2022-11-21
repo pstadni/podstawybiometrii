ca3be39cb7089f45a74a89ef64582acd803faf67
pstadni <103567424+pstadni@users.noreply.github.com>
2022-11-21 21:56:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import dlib
 import math
+import tkinter
 
 # Wczytanie detectora
 detector = dlib.get_frontal_face_detector()
@@ -17,10 +18,10 @@ gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
 # Używanie detectora do znalezienia landmarks
 faces = detector(gray)
 for face in faces:
-    x1 = face.left() # lewo
-    y1 = face.top() # góra
-    x2 = face.right() # prawo
-    y2 = face.bottom() # dół
+    x1 = face.left()            # lewo
+    y1 = face.top()             # góra
+    x2 = face.right()           # prawo
+    y2 = face.bottom()          # dół
 
     # Tworzenie landmarku
     landmarks = predictor(image=gray, box=face)
@@ -31,6 +32,10 @@ for face in faces:
         y = landmarks.part(n).y
         cv2.circle(img=img, center=(x, y), radius=3, color=(0, 255, 0), thickness=-1)
 
+    def odl(x,y):
+        math.dist(x, y)
+
+
     xa = landmarks.part(36).x #prawe oko zew
     ya = landmarks.part(36).y
     praweokozew = [xa,ya]
@@ -39,8 +44,7 @@ for face in faces:
     yb = landmarks.part(45).y
     leweokozew = [xb, yb]
 
-    odl1= math.dist(praweokozew,leweokozew) #odległość między zewnętrznymi kącikami oczu
-    print(odl1)
+    odl1 =  math.dist(praweokozew,leweokozew) #odległość między zewnętrznymi kącikami oczu
 
     xc = landmarks.part(39).x  # prawe oko wew
     yc = landmarks.part(39).y
@@ -51,11 +55,9 @@ for face in faces:
     leweokowewn = [xd, yd]
 
     odl2 = math.dist(praweokowewn, leweokowewn)  # odległość między wewnętrznymi kącikami oczu
-    print(odl2)
-
     stosunek1 = abs(odl1/odl2)
 
-    print('Stosunek odległości kącików oczu od siebie '+ str(stosunek1))
+    print('Stosunek 1: '+ str(stosunek1))
 
 
 #52 do 8 to dolny brzeg ust do brody
@@ -69,7 +71,6 @@ for face in faces:
     broda = [xf, yf]
 
     odl3 = math.dist(dolust, broda)  # odleglosc od ust do brody
-    print(odl3)
 
 # 6 i 10 szerokosc ust
 
@@ -82,11 +83,60 @@ for face in faces:
     usta2 = [xh, yh]
 
     odl4 = math.dist(usta1, usta2)  # wybrana szerokosc brody
-    print(odl4)
 
     stosunek2 = abs(odl3/odl4)
-    print('Stosunek szerokości ust do odległości ust od brody '+ str(stosunek2))
+    print('Stosunek 2: '+ str(stosunek2))
+
+    xj = landmarks.part(0).x
+    yj = landmarks.part(0).y
+    z1 = [xj, yj]
+
+    xl = landmarks.part(16).x
+    yl = landmarks.part(16).y
+    z2 = [xl, yl]
+
+    odl5=math.dist(z1,z2)
+
+    xm = landmarks.part(27).x
+    ym = landmarks.part(27).y
+    z3 = [xm, ym]
+
+
+    xn = landmarks.part(28).x
+    yn = landmarks.part(28).y
+    z4 = [xn, yn]
+
+    odl6=math.dist(z3,z4)
+
+    stosunek3=abs(odl5/odl6)
+    print("Stosunek 3: "+ str(stosunek3))
+
+    xo = landmarks.part(17).x
+    yo = landmarks.part(17).y
+    z5 = [xo, yo]
+
+    xp = landmarks.part(26).x
+    yp = landmarks.part(26).y
+    z6 = [xp, yp]
+
+    odl7 = math.dist(z5, z6)
+
+    xr = landmarks.part(21).x
+    yr = landmarks.part(21).y
+    z7 = [xr, yr]
+
+    xs = landmarks.part(22).x
+    ys = landmarks.part(22).y
+    z8 = [xs, ys]
+
+    odl8 = math.dist(z7, z8)
+
+    stosunek4 = abs(odl7 / odl8)
+    print("Stosunek 4: " + str(stosunek4))
+
 
 cv2.imshow(winname="Face", mat=img)
 cv2.waitKey(delay=0)
 cv2.destroyAllWindows()
+
+
